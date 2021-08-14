84403f0ec0c74ef2e3ca68c54c4b3904e1dbe812
Shingo Suzuki <suzushinlab@gmail.com>
2021-08-15 08:29:55 +0900

    ver 1.00
    Public Release!

---
+++ PageLinker.js
@@ -0,0 +1,5 @@
+// Author: Shingo Suzuki (@suzushin7)
+javascript: (function (t, u) {
+  prompt('PageLinker', '<a href="' + u + '" title="' + t + '">' + t + '</a>');
+  void (0);
+})(document.title, location.href);
